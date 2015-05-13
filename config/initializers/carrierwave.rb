b383cef6a6d4b1c64f99bbf8b79b994c2e01a682
astijus <astijus@daliserdves.com>
2015-05-13 13:41:56 +0000

    h

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    :aws_access_key_id      => ENV["AKIAJOPHTHW7FYTQC4MA"],                        # required
-    :aws_secret_access_key  => ENV["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"],                        # required
+    :aws_access_key_id      => ENV["AKIAJXH7VEOHDDDSFTJA"],                        # required
+    :aws_secret_access_key  => ENV["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"],                        # required
     :region                 => 'us-east-1'
   }
   config.fog_directory  = ENV["astijusyelpdemo"]                    # required
